1b2db2c98c00fe11b808e81dfd0b5d1b7d4133ae
drone <drone@drone.com>
2016-03-16 15:37:54 +0000

    drone side

--- drone/api.js
+++ drone/api.js
@@ -5,13 +5,27 @@ var API = module.exports = {
     'welcome' : function() {
     },
     'cmd:rc:pwm' : function(data) {
-      console.log(data);
-      //console.log(this.welcomeMessage);
-      var perc= (data.val / 32767) * 4000;
+	var perc = Math.floor((data.val / (32767 * 2)) * 1800);
 
-      if (perc < 0) perc *= -1;
-      console.log(perc);
-      //this.pwm.setPulseLength(4, perc);
+	if (perc < 0) perc *= -1;
+	
+	console.log('bef',perc);
+	perc += 900;
+	// max 564
+	if (data.act == 'pitch') {
+	    console.log(perc);
+	    //console.log((1.12 * 4096) / (1000 / 60) -1)
+	    //console.log((2.3 * 4096) / (1000 / 60) -1)
+	    this.pwm.setPWM(6, perc);
+	}
+
+	if (data.act == 'roll') {
+	    this.pwm.setPWM(7, perc);
+	}
+
+	if (data.act == 'yaw') {
+	    this.pwm.setPWM(8, perc);
+	}
 
       //console.log(perc);
     }
--- drone/lib/pwm.js
+++ drone/lib/pwm.js
@@ -9,7 +9,7 @@ class PWM {
     var options = {
 	    i2c       : i2cBus.openSync(1),
 	    address   : 0x40,
-	    frequency : 60,
+	    frequency : 50,
 	    debug     : false
     };
 
@@ -17,18 +17,26 @@ class PWM {
     try {
       fs.appendFileSync('/sys/class/gpio/export', '27');
     } catch(e) {
-      //console.error(e);
+      console.error(e);
     }
 
+      var dt = fs.readFileSync('/sys/class/gpio/gpio27/direction')
+      console.log(dt.toString());
+
+      
     try {
-      fs.writeFileSync('/sys/class/gpio/gpio27/value', '0');
+	// Set pin as output?
+	fs.writeFileSync('/sys/class/gpio/gpio27/direction', 'out');
+	fs.writeFileSync('/sys/class/gpio/gpio27/value', '0');
     } catch(e) {
-      //console.error(e);
+      console.error(e);
     }
 
     this.pwm = new Pca9685Driver(options, function() {
 	    console.log('PWM initialization done');
     });
+
+      return this;
   }
 
   /**
@@ -36,10 +44,17 @@ class PWM {
    */
   setPWM(chanel, pulse) {
     this.pwm.setPulseLength(chanel, pulse);
-
     // Set channel 0 to turn on on step 42 and off on step 255
     //pwm.setPulseRange(4, 0, 0);
   }
+
+    armESC() {
+	var self = this;
+
+	(function arm(i)  {
+	    //self.setPWM( MIN (1834, 1.12ms) 50 times
+	})(50)
+    }
 }
 
 module.exports = PWM;
